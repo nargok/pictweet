044ac109f9cddc8f8b4f903c80c61fc87d2ef5f9
後閑諒一 <nargok@gmail.com>
2017-09-09 15:21:50 +0900

    tweet投稿画面を実装

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -2,9 +2,44 @@ require 'rails_helper'
 
 RSpec.describe TweetsController, :type => :controller do
 
-  it 'returns tweets object with #index' do
-    get :index
-    expect(assigns(:tweets)).not_to be_nil
+  describe 'GET #index' do
+    it 'returns tweets object with #index' do
+      get :index
+      expect(assigns(:tweets)).not_to be_nil
+    end
+
+    #index templateを描画する
+    it 'renders index template' do
+      get :index
+      expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #new' do
+    #new templateを描画する
+    it 'renders new template' do
+      get :new
+      expect(response).to render_template :new
+    end
+  end
+
+
+  describe 'POST #create' do
+    # 有効な属性の場合
+    context 'with valid attributes' do
+      # databaseに新しいtweetを保存すること
+      it 'creates new tweets in the database' do
+        expect{
+          post :create, tweet: build(:tweet)
+        }.to change(Tweet, :count).by(1)
+      end
+      # createビューを描画すること
+      it 'renders create view template' do
+        post :create, tweet: build(:tweet)
+        expect(response).to render_template :create
+      end
+
+    end
   end
 
 end
